5f6958110cb6af95523c37d0eee5660e207676c0
wangkai <1137120948@qq.com>
2020-12-02 16:01:56 +0800

    first-react

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-02 15:26:26
+ * @LastEditTime: 2020-12-02 15:27:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /MyNote/vue-react/src/index.js
+ */
+
+import CreateReactApp from './react-app'
+
+CreateReactApp()
